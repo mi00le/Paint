31601b7c9277c624781646369c41f64ba15fc958
michael <michael.lindfors@hotmail.com>
2019-01-16 15:05:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,115 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './sketch.css';
+import {SketchField, Tools} from 'react-sketch';
+import { SketchPicker } from 'react-color'
+import reactCSS from 'reactcss'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      background: 'FFF',
+      displayColorPicker: false,
+      isHidden : true
+    }
+  }
+
+  toggleHidden = () => {
+    this.setState({
+      isHidden: !this.state.isHidden
+    })
+  };
+
+  //display color picker
+  handleClick = () => {
+    this.setState({ displayColorPicker: !this.state.displayColorPicker })
+  };
+  //hide color picker
+  handleClose = () => {
+    this.setState({ displayColorPicker: false })
+  };
+
+  handleChangeComplete = (color) => {
+    this.setState({ background: color.hex });
+  };
+  
+
   render() {
+    const popover = {
+      position: 'absolute',
+      zIndex: '2',
+    }
+    const cover = {
+      position: 'fixed',
+      top: '0px',
+      right: '0px',
+      bottom: '0px',
+      left: '0px',
+    }
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar handleClick={this.handleClick} toggleHidden={this.toggleHidden} isHidden={this.state.isHidden}/>
+        { this.state.displayColorPicker ? <div style={ popover }>
+          <div style={ cover } onClick={ this.handleClose }/>
+        <SketchPicker color={ this.state.background } onChangeComplete={ this.handleChangeComplete }/>
+    </div> : null}
+    <SketchFieldDemo color={ this.state.background }/>
       </div>
     );
+  }
+}
+
+
+
+class SketchFieldDemo extends React.Component {
+
+  
+  render() {
+     return (
+       
+         <SketchField width='1024px' 
+                      height='768px' 
+                      tool={Tools.Pencil} 
+                      lineColor={this.props.color}
+                      lineWidth={3}
+                      />
+     )
+  }
+}
+
+
+
+class Navbar extends React.Component {
+
+  render(){
+    return(
+
+
+      <div className="dropdown">
+      <button className="dropbtn">Tools</button>
+      <div className="dropdown-content">
+
+      <a href="#colorPicker" className="toggle-color" onClick={ this.props.handleClick }>Pick Color</a>
+      <a href="#" className="more-pencil" onClick={this.props.toggleHidden}>Pencil</a>
+     
+      {!this.props.isHidden && (<ul>
+            <a href="">Pencil</a>
+            <a href="">Line</a>
+            <a href="">Rectangle</a>
+            <a href="">Circle</a>
+            <a href="">Select</a>
+            <a href="">Move</a>
+        </ul>
+      )}
+      <a href="#">Linewidth</a>
+      </div>
+    </div>
+    )
   }
 }
 
