5525f12cef94f4c5132a8212e8933edf7cee0c5f
michael <michael.lindfors@hotmail.com>
2019-01-17 15:57:16 +0200

    handleclick

--- src/App.js
+++ src/App.js
@@ -11,9 +11,17 @@ class App extends Component {
     super(props);
 
     this.state = {
-      background: 'FFF',
+      penColor: {
+        r : 0,
+        g : 0,
+        b : 0,
+        a : 100
+      },
       displayColorPicker: false,
-      isHidden : true
+      isHidden : true,
+      penSize : 10,
+      toolType : "pencil",
+      toggleTransparent : true
     }
   }
 
@@ -31,11 +39,20 @@ class App extends Component {
   handleClose = () => {
     this.setState({ displayColorPicker: false })
   };
-
+  //after color is picked
   handleChangeComplete = (color) => {
-    this.setState({ background: color.hex });
+    
+    this.setState({ penColor: color.rgb});
+    console.log(color.rgb,color.hex);
   };
+  //pensize handler
+  handlePenSize = (penSize) => {this.setState({penSize})}
   
+  //toolType handler
+  handleToolType = (toolType) => { this.setState({toolType})} 
+
+  //toggle transparent fill
+  handleTransparent = () => { this.setState({ toggleTransparent : !this.state.toggleTransparent })}
 
   render() {
     const popover = {
@@ -53,13 +70,14 @@ class App extends Component {
 
     return (
       <div className="App">
-      <ToolBar/>
-        <Navbar handleClick={this.handleClick} toggleHidden={this.toggleHidden} isHidden={this.state.isHidden}/>
+      
+        {/* <Navbar handleClick={this.handleClick} toggleHidden={this.toggleHidden} isHidden={this.state.isHidden}/> */}
+        <ToolBar handleTransparent={ this.handleTransparent } handleClick={this.handleClick} handlePenSize={ this.handlePenSize } handleToolType={ this.handleToolType }/>
         { this.state.displayColorPicker ? <div style={ popover }>
           <div style={ cover } onClick={ this.handleClose }/>
-        <SketchPicker color={ this.state.background } onChangeComplete={ this.handleChangeComplete }/>
+        <SketchPicker color={ this.state.penColor } onChangeComplete={ this.handleChangeComplete }/>
     </div> : null}
-    <SketchFieldDemo color={ this.state.background }/>
+    <SketchFieldDemo transparent={this.state.toggleTransparent}  color={ this.state.penColor } size={ this.state.penSize } types={ this.state.toolType }/>
       </div>
     );
   }
@@ -70,52 +88,56 @@ class App extends Component {
 class SketchFieldDemo extends React.Component {
 
   
+  
   render() {
      return (
        
          <SketchField width='800px' 
                       height='700px' 
-                      tool={Tools.Pencil} 
-                      lineColor={this.props.color}
-                      lineWidth={3}
+                      tool={this.props.types} 
+                      lineColor={`rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},${this.props.color.a})`}
+                      lineWidth={this.props.size}
                       backgroundColor='white'
+                      undoSteps={2}
+                      fillColor={this.props.transparent ? "transparent" : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`}
                       style={{position:"relative",margin:"auto",top:"20px"}}
                       />
+                      // this.props.color.a < 1 ? ("transparent",console.log(this.props.color.a)) : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`
      )
   }
 }
 
 
 
-class Navbar extends React.Component {
+// class Navbar extends React.Component {
 
-  render(){
-    return(
+//   render(){
+//     return(
 
 
-      <div className="dropdown">
-      <button className="dropbtn">Tools</button>
-      <div className="dropdown-content">
+//       <div className="dropdown">
+//       <button className="dropbtn">Tools</button>
+//       <div className="dropdown-content">
 
-      <a href="#colorPicker" className="toggle-color" onClick={ this.props.handleClick }>Pick Color</a>
-      <a href="#morePencils" className="more-pencil" onClick={this.props.toggleHidden}>Pencil</a>
+//       <a href="#colorPicker" className="toggle-color" onClick={ this.props.handleClick }>Pick Color</a>
+//       <a href="#morePencils" className="more-pencil" onClick={this.props.toggleHidden}>Pencil</a>
      
-      {!this.props.isHidden && (<ul>
-            <a href="#pencil">Pencil</a>
-            <a href="#line">Line</a>
-            <a href="#rectangle">Rectangle</a>
-            <a href="#circle">Circle</a>
-            <a href="#select">Select</a>
-            <a href="#move">Move</a>
-        </ul>
-      )}
-      <a href="#linewidth">Linewidth</a>
-      </div>
+//       {!this.props.isHidden && (<ul>
+//             <a href="#pencil">Pencil</a>
+//             <a href="#line">Line</a>
+//             <a href="#rectangle">Rectangle</a>
+//             <a href="#circle">Circle</a>
+//             <a href="#select">Select</a>
+//             <a href="#move">Move</a>
+//         </ul>
+//       )}
+//       <a href="#linewidth">Linewidth</a>
+//       </div>
       
-    </div>
+//     </div>
 
-    )
-  }
-}
+//     )
+//   }
+// }
 
 export default App;
--- src/Components/ToolBar/index.js
+++ src/Components/ToolBar/index.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
-import { FaPen, FaSquare, FaCircle, FaMinus, FaRedo, FaUndo } from 'react-icons/fa';
+import { FaPaintBrush, FaSquare, FaCircle, FaMinus, FaRedo, FaUndo, FaFillDrip } from 'react-icons/fa';
 import { IconContext } from "react-icons";
 
 export default class Toolbar extends React.Component {
@@ -17,29 +17,30 @@ export default class Toolbar extends React.Component {
                 </Navbar.Header>
                 <Navbar.Collapse>
                     <Nav>
-                        <NavDropdown eventKey={1} title="Pencil" id="basic-nav-dropdown">
-                            <MenuItem eventKey={1.1}><FaPen></FaPen></MenuItem>
-                            <MenuItem eventKey={1.3}><FaMinus></FaMinus></MenuItem>
-                            <MenuItem eventKey={1.2}><FaSquare></FaSquare></MenuItem>
-                            <MenuItem eventKey={1.3}><FaCircle></FaCircle></MenuItem>
+                        <NavDropdown eventKey={1} title="Tools" id="basic-nav-dropdown">
+                            <MenuItem eventKey={1.1} onClick={() => this.props.handleToolType("pencil")}><FaPaintBrush></FaPaintBrush></MenuItem>
+                            <MenuItem eventKey={1.2} onClick={() => this.props.handleToolType("line")}><FaMinus></FaMinus></MenuItem>
+                            <MenuItem eventKey={1.3} onClick={() => this.props.handleToolType("rectangle")}><FaSquare></FaSquare></MenuItem>
+                            <MenuItem eventKey={1.4} onClick={() => this.props.handleToolType("circle")}><FaCircle></FaCircle></MenuItem>
+                            <MenuItem eventKey={1.5} onClick={this.props.handleTransparent}><FaFillDrip></FaFillDrip></MenuItem>
                             {/* <MenuItem divider />
                             <MenuItem eventKey={1.3}>Separated link</MenuItem> */}
                         </NavDropdown>
-                        <NavItem eventKey={2} href="#">
+                        <NavItem eventKey={2} href="#color" onClick={ this.props.handleClick }>
                             Color
                         </NavItem>
                         <NavDropdown eventKey={3} title="Pen Size" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1} onClick={()=> console.log("3")}><IconContext.Provider value={{ color: "black",size : "2em", className: "global-class-name" }}>
+                            <MenuItem eventKey={3.1} onClick={ () => this.props.handlePenSize(30) }><IconContext.Provider value={{ color: "black",size : "2em", className: "global-class-name" }}>
                                 <div>
                                     <FaCircle />
                                 </div>
                             </IconContext.Provider></MenuItem>
-                            <MenuItem eventKey={3.2} onClick={()=> console.log("2")}><IconContext.Provider value={{ color: "black",size : "1.5em", className: "global-class-name" }}>
+                            <MenuItem eventKey={3.2} onClick={ () => this.props.handlePenSize(20)}><IconContext.Provider value={{ color: "black",size : "1.5em", className: "global-class-name" }}>
                                 <div>
                                     <FaCircle />
                                 </div>
                             </IconContext.Provider></MenuItem>
-                            <MenuItem eventKey={3.3} onClick={()=> console.log("1")}><IconContext.Provider value={{ color: "black",size : "1em", className: "global-class-name" }}>
+                            <MenuItem eventKey={3.3} onClick={ () => this.props.handlePenSize(10)}><IconContext.Provider value={{ color: "black",size : "1em", className: "global-class-name" }}>
                                 <div>
                                     <FaCircle />
                                 </div>
