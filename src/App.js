3ab06f97b368552ba992e38b14ec323f286e2a1a
michael <michael.lindfors@hotmail.com>
2019-01-21 15:25:55 +0200

    before login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import './sketch.css';
-import {SketchField, Tools} from 'react-sketch';
+import './Css/App.css';
+import './Css/sketch.css';
+import { SketchField, Tools } from 'react-sketch';
 import { SketchPicker } from 'react-color'
 import reactCSS from 'reactcss'
 import ToolBar from './Components/ToolBar/index.js';
 import dataJsoncontrolled from './Components/JSON/layer.js';
 import words from './Components/JSON/words/index.js';
 import Clock from './Components/Timer';
-import MainScreen from './Components/mainScreen';
+import Login from './Components/Login';
 
 
 let arr = [];
@@ -18,67 +18,55 @@ arr.push(dataJsoncontrolled);
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      enter : false,
-      penColor: { r : 0, g : 0, b : 0, a : 100},
+      enter: false,
+      penColor: { r: 0, g: 0, b: 0, a: 100 },
       displayColorPicker: false,
-      isHidden : true,
-      penSize : 10,
-      toolType : "pencil",
-      toggleTransparent : true,
-      clearCanvas : 'transparent',
-      shouldClear : false
+      isHidden: true,
+      penSize: 10,
+      toolType: "pencil",
+      toggleTransparent: true,
+      clearCanvas: 'transparent',
+      shouldClear: false
     }
   }
 
-  handleMain = () => {
-    
-    this.setState({
-      enter : true
-    })
-  }
+  //handle Login 
+  handleLogin = () => { this.setState({ enter: true }) }
 
-  toggleHidden = () => {
-    this.setState({
-      isHidden: !this.state.isHidden
-    })
-  };
+  //toggle hidden
+  toggleHidden = () => { this.setState({ isHidden: !this.state.isHidden }) };
 
   //display color picker
-  handleClick = () => {
-    this.setState({ displayColorPicker: !this.state.displayColorPicker, shouldClear : false })
-  };
+  handleClick = () => { this.setState({ displayColorPicker: !this.state.displayColorPicker, shouldClear: false }) };
+
   //hide color picker
-  handleClose = () => {
-    this.setState({ displayColorPicker: false, shouldClear : false })
-  };
+  handleClose = () => { this.setState({ displayColorPicker: false, shouldClear: false }) };
+
   //after color is picked
-  handleChangeComplete = (color) => {
-    
-    this.setState({ penColor: color.rgb});
-  };
+  handleChangeComplete = (color) => { this.setState({ penColor: color.rgb }) };
+
   //pensize handler
-  handlePenSize = (penSize) => {this.setState({penSize, shouldClear : false})}
-  
+  handlePenSize = (penSize) => { this.setState({ penSize, shouldClear: false }) }
+
   //toolType handler
-  handleToolType = (toolType) => { this.setState({toolType, penColor : {r : 0, g : 0, b : 0, a : 100}, shouldClear : false})} 
+  handleToolType = (toolType) => { this.setState({ toolType, penColor: { r: 0, g: 0, b: 0, a: 100 }, shouldClear: false }) }
 
   //toggle transparent fill
-  handleTransparent = () => { this.setState({ toggleTransparent : !this.state.toggleTransparent, shouldClear : false })}
+  handleTransparent = () => { this.setState({ toggleTransparent: !this.state.toggleTransparent, shouldClear: false }) }
 
   //handle clear canvas
-  handleClear = () => { this.setState({ shouldClear : true })};
+  handleClear = () => { this.setState({ shouldClear: true }) };
 
   //Eraser..
-  handleEraser = () => {this.setState({ penColor : { r : 255, g : 255, b : 255, a : 100 }, penSize : 30, toolType : 'pencil', shouldClear : false})}
+  handleEraser = () => { this.setState({ penColor: { r: 255, g: 255, b: 255, a: 100 }, penSize: 30, toolType: 'pencil', shouldClear: false }) }
 
-  
 
   render() {
-    
+
     const popover = {
       position: 'absolute',
       zIndex: '2',
@@ -90,20 +78,30 @@ class App extends Component {
       bottom: '0px',
       left: '0px',
 
-      
+
     }
-    let childArr =[ <ToolBar key={1} onSketchChange = {this.onSketchChange} handleTransparent={ this.handleTransparent } 
-    handleClick={this.handleClick} handlePenSize={ this.handlePenSize } handleToolType={ this.handleToolType } 
-    handleClear={this.handleClear} handleEraser={this.handleEraser}/>, this.state.displayColorPicker ? (<div style={ popover }>
-          <div style={ cover } key={2} onClick={ this.handleClose }/>
-        <SketchPicker key={3} color={ this.state.penColor } onChangeComplete={ this.handleChangeComplete }/>
-    </div>) : null,<Clock key={4}/>,<SketchFieldDemo key={5} clearBoolean={this.state.shouldClear} transparent={this.state.toggleTransparent} reset={ this.state.clearCanvas }  color={ this.state.penColor } size={ this.state.penSize } types={ this.state.toolType }/>];
+    let childArr = [
+      <ToolBar key={1} onSketchChange={this.onSketchChange} handleTransparent={this.handleTransparent}
+        handleClick={this.handleClick} handlePenSize={this.handlePenSize} handleToolType={this.handleToolType}
+        handleClear={this.handleClear} handleEraser={this.handleEraser} />,
+
+      this.state.displayColorPicker ? (
+        <div style={popover}>
+          <div style={cover} key={2} onClick={this.handleClose} />
+          <SketchPicker key={3} color={this.state.penColor} onChangeComplete={this.handleChangeComplete} />
+        </div>) : null,
+
+      <Clock key={4} />,
+
+      <SketchFieldDemo key={5} clearBoolean={this.state.shouldClear} transparent={this.state.toggleTransparent}
+        reset={this.state.clearCanvas} color={this.state.penColor} size={this.state.penSize} types={this.state.toolType} />
+    ];
 
     return (
       <div className="App">
-        {(!this.state.enter) ? (<MainScreen handleMain={this.handleMain}/> ) : (
-          childArr)} 
-          </div>
+        {(!this.state.enter) ? (<Login handleLogin={this.handleLogin} />) : (
+          childArr)}
+      </div>
     );
   }
 }
@@ -111,24 +109,24 @@ class App extends Component {
 
 class SketchFieldDemo extends React.Component {
 
-  
+
   render() {
     //make a copy of arr to store for later
     let a = arr.slice(0);
-     return (
-       
-         <SketchField width='800px' 
-                      height='700px' 
-                      tool={this.props.types} 
-                      lineColor={`rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},${this.props.color.a})`}
-                      lineWidth={this.props.size}
-                      backgroundColor={!this.props.clearBoolean ? 'transparent' : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`}
-                      undoSteps={2}
-                      fillColor={this.props.transparent ? "transparent" : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`}
-                      style={{position:"relative",margin:"auto",top:"20px"}}
-                      value={this.props.clearBoolean ? a : null}
-                      />
-     )
+    return (
+
+      <SketchField width='800px'
+        height='700px'
+        tool={this.props.types}
+        lineColor={`rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},${this.props.color.a})`}
+        lineWidth={this.props.size}
+        backgroundColor={!this.props.clearBoolean ? 'transparent' : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`}
+        undoSteps={2}
+        fillColor={this.props.transparent ? "transparent" : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`}
+        style={{ position: "relative", margin: "auto", top: "20px" }}
+        value={this.props.clearBoolean ? a : null}
+      />
+    )
   }
 }
 
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 
 
 
-export default class MainScreen extends React.Component{
+export default class Login extends React.Component{
 
 
     render(){
         return(
             <div style={{width:"100%",height : "100%"}}>
             <input type="text" placeholder="Enter a username.."></input>
-            <button onClick={this.props.handleMain}>Enter!</button>
+            <button onClick={this.props.handleLogin}>Enter!</button>
             </div>
         )
     }
