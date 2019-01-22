f93eef9f8a82600867feef633b2440525147af48
Michael Lindfors <31505546+mi00le@users.noreply.github.com>
2019-01-22 17:19:01 +0200

    Update index.js

--- src/Components/firebase/index.js
+++ src/Components/firebase/index.js
@@ -1,16 +1,4 @@
 import * as firebase from 'firebase';
  
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBI9NIuZPn0-Lz5x1Q6KHQi-waUBo8YK0Y",
-    authDomain: "painter-4b28e.firebaseapp.com",
-    databaseURL: "https://painter-4b28e.firebaseio.com",
-    projectId: "painter-4b28e",
-    storageBucket: "painter-4b28e.appspot.com",
-    messagingSenderId: "188103321319"
-  };
-
-firebase.initializeApp(config);
-
 
 export default firebase;
