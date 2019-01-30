b3475c1ed50c2e844e3757b2ed06b38193c542f1
michael <michael.lindfors@hotmail.com>
2019-01-30 15:18:06 +0200

    separated component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './Css/App.css';
 import './Css/sketch.css';
 import './Css/register.css';
 import './Css/toolbar.css';
-import { SketchField } from 'react-sketch';
 import { SketchPicker } from 'react-color'
-import ToolBar from './Components/ToolBar/index.js';
-import dataJsoncontrolled from './Components/JSON/layer.js';
-// import words from './Components/JSON/words/index.js';
-// import Clock from './Components/Timer';
 import LoginScreen from './Components/LoginScreen';
+import ToolBar from './Components/ToolBar';
+import SketchFieldDemo from './Components/SketchFieldDemo';
 import firebase from './Components/firebase';
-
-
-
-
-let arr = [];
-
-arr.push(dataJsoncontrolled);
 
 
 class App extends Component {
@@ -140,28 +130,7 @@ class App extends Component {
 }
 
 
-class SketchFieldDemo extends React.Component {
 
-
-  render() {
-    //make a copy of arr to store for later --> 
-    let a = arr.slice(0);
-    return (
-
-      <SketchField width='800px'
-        height='700px'
-        tool={this.props.types}
-        lineColor={`rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},${this.props.color.a})`}
-        lineWidth={this.props.size}
-        backgroundColor={!this.props.clearBoolean ? 'transparent' : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`}
-        undoSteps={2}
-        fillColor={this.props.transparent ? "transparent" : `rgba(${this.props.color.r},${this.props.color.g},${this.props.color.b},100)`}
-        style={{ position: "relative", margin: "auto", top: "20px" }}
-        value={this.props.clearBoolean ? a : null}
-      />
-    )
-  }
-}
 
 
 
